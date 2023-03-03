67f37179e7f6ae94fa1151f2a377349d5f6c41db
Leonid <leo-turko@mail.ru>
2023-03-03 22:21:26 +0300

    SingleImageView settings

---
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Леонид Турко on 03.03.2023.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+  @IBOutlet weak var imageView: UIImageView!
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
