7fdea3496373f0afc5ca670691d256fcdcc4abbf
Leonid <leo-turko@mail.ru>
2023-03-03 23:23:40 +0300

    Navigation-controller segue solution

--- ImageFeed/ImageList/ImagesListViewController.swift
+++ ImageFeed/ImageList/ImagesListViewController.swift
@@ -8,33 +8,34 @@
 import UIKit
 
 class ImagesListViewController: UIViewController {
-    //  MARK: - IB Outlets
-    @IBOutlet private var tableView: UITableView!
-    
-    //  MARK: - Private Properties
-    private let photosName: [String] = (0..<20).map { "\($0)" }
-
-    private lazy var dateFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .long
-        formatter.timeStyle = .none
-        return formatter
-    }()
-    
-    override var preferredStatusBarStyle: UIStatusBarStyle { .lightContent }
-
-    //  MARK: - Override Methods
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
-    }
+  //  MARK: - IB Outlets
+  @IBOutlet private var tableView: UITableView!
+  
+  //  MARK: - Private Properties
+  private let photosName: [String] = (0..<20).map { "\($0)" }
+  private let ShowSingleImageSegueIdentifier = "ShowSingleImage"
+  
+  private lazy var dateFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .long
+    formatter.timeStyle = .none
+    return formatter
+  }()
+  
+  override var preferredStatusBarStyle: UIStatusBarStyle { .lightContent }
+  
+  //  MARK: - Override Methods
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+  }
   
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    if segue.identifier == "ShowSingleImage" {
+    if segue.identifier == ShowSingleImageSegueIdentifier {
       guard let viewController = segue.destination as? SingleImageViewController else { return }
       guard let indexPath = sender as? IndexPath else { return }
       let image = UIImage(named: photosName[indexPath.row])
-      viewController.imageView.image = image
+      viewController.image = image
     } else {
       super.prepare(for: segue, sender: sender)
     }
@@ -43,52 +44,51 @@ class ImagesListViewController: UIViewController {
 
 //  MARK: - UITableViewDataSource
 extension ImagesListViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        photosName.count
-    }
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    photosName.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: ImageListCell.reuseIdentifier, for: indexPath)
+    guard let imageListCell = cell as? ImageListCell else { return UITableViewCell() }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: ImageListCell.reuseIdentifier, for: indexPath)
-        guard let imageListCell = cell as? ImageListCell else { return UITableViewCell() }
-      
-        configCell(for: imageListCell, with: indexPath)
-        return imageListCell
-    }
+    configCell(for: imageListCell, with: indexPath)
+    return imageListCell
+  }
 }
 
 //  MARK: - UITableViewDelegate
 extension ImagesListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-      performSegue(withIdentifier: "showSingleImage", sender: indexPath)
-    }
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+    performSegue(withIdentifier: ShowSingleImageSegueIdentifier, sender: indexPath)
+  }
+  
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    guard let image = UIImage(named: photosName[indexPath.row]) else { return 0 }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        guard let image = UIImage(named: photosName[indexPath.row]) else { return 0 }
-        
-        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
-        let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
-        let imageWidth = image.size.width
-        let scale = imageViewWidth / imageWidth
-        let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
-        return cellHeight
-    }
-  
-  
+    let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+    let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+    let imageWidth = image.size.width
+    let scale = imageViewWidth / imageWidth
+    let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
+    return cellHeight
+  }
 }
 
 //  MARK: -  Private Methods
 extension ImagesListViewController {
-    private func configCell(for cell: ImageListCell, with indexPath: IndexPath) {
-        guard let image = UIImage(named: photosName[indexPath.row]) else { return }
-        cell.pictureImage.image = image
-        cell.dataLabel.text = dateFormatter.string(from: Date())
-        let isLiked = indexPath.row % 2 == 0
-        let likeImage = isLiked
-        ? UIImage(named: "like_button_on")
-        : UIImage(named: "like_button_off")
-        cell.likeButton.setImage(likeImage, for: .normal)
-        
-    }
+  private func configCell(for cell: ImageListCell, with indexPath: IndexPath) {
+    guard let image = UIImage(named: photosName[indexPath.row]) else { return }
+    cell.pictureImage.image = image
+    cell.dataLabel.text = dateFormatter.string(from: Date())
+    let isLiked = indexPath.row % 2 == 0
+    let likeImage = isLiked
+    ? UIImage(named: "like_button_on")
+    : UIImage(named: "like_button_off")
+    cell.likeButton.setImage(likeImage, for: .normal)
+    
+  }
 }
 
 
--- ImageFeed/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 final class SingleImageViewController: UIViewController {
   @IBOutlet weak var imageView: UIImageView!
   
+  var image: UIImage! {
+    didSet {
+      guard isViewLoaded else { return }
+      imageView.image = image
+    }
+  }
+  
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+      imageView.image = image
     }
     
 
