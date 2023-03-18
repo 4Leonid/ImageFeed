2e82f31913d367d0d19b72822210401b3ea93fcc
Leonid <leo-turko@mail.ru>
2023-03-18 22:27:52 +0300

    Update some code after review

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -9,8 +9,57 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
   //  MARK: - Private Properties
-  private var avatarImageView: UIImageView?
-
+  private lazy var avatarImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    let profileImage = UIImage(named: "avatar")
+    imageView.image = profileImage
+    return imageView
+  }()
+  
+  private lazy var logoutButton: UIButton = {
+    let button = UIButton()
+    button.translatesAutoresizingMaskIntoConstraints = false
+    let buttonImage = UIImage(systemName: "ipad.and.arrow.forward")
+    button.setImage(buttonImage, for: .normal)
+    button.addTarget(self, action: #selector(didTapLogoutButton), for: .touchUpInside)
+    button.tintColor = .ypRed
+    return button
+  }()
+  
+  private lazy var stackView: UIStackView = {
+    let stackView = UIStackView()
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    stackView.axis = .vertical
+    stackView.spacing = 8
+    stackView.distribution = .fill
+    return stackView
+  }()
+  
+  private lazy var nameLabel: UILabel = {
+    let label = UILabel()
+    label.text = "Екатерина Новикова"
+    label.textColor = .white
+    label.font = UIFont.systemFont(ofSize: 23, weight: .bold)
+    return label
+  }()
+  
+  private lazy var loginNameLabel: UILabel = {
+    let label = UILabel()
+    label.text = "@ekaterina_nov"
+    label.textColor = .ypGray
+    label.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+    return label
+  }()
+  
+  private lazy var descriptionLabel: UILabel = {
+    let label = UILabel()
+    label.text = "Hello, world!"
+    label.textColor = .white
+    label.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+    return label
+  }()
+  
   //  MARK: - Public Properties
   override var preferredStatusBarStyle: UIStatusBarStyle { .lightContent }
   
@@ -18,9 +67,9 @@ final class ProfileViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .ypBlack
-    setupImage()
-    setupButton()
-    setupLabels()
+    addViews()
+    activateConstraints()
+
   }
   
   //  MARK: - Public Methods
@@ -29,71 +78,24 @@ final class ProfileViewController: UIViewController {
 
 //  MARK: -  Private Methods
 extension ProfileViewController {
-  private func setupImage() {
-    guard let profileImage = UIImage(named: "avatar") else { return }
-    let imageView = UIImageView()
-    imageView.image =  profileImage
-    imageView.translatesAutoresizingMaskIntoConstraints = false
-    view.addSubview(imageView)
-    self.avatarImageView = imageView
-    
-    NSLayoutConstraint.activate([
-      imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
-      imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16)
-    ])
-  }
-  
-  private func setupButton() {
-    guard let buttonImage = UIImage(systemName: "ipad.and.arrow.forward") else { return }
-    guard let avatarImageView = avatarImageView else { return }
- 
-    let button = UIButton.systemButton(
-      with: buttonImage,
-      target: self,
-      action: #selector(didTapLogoutButton)
-    )
-    button.tintColor = .ypRed
-    button.translatesAutoresizingMaskIntoConstraints = false
-    view.addSubview(button)
-    
-    NSLayoutConstraint.activate([
-      button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
-      button.centerYAnchor.constraint(equalTo: avatarImageView.centerYAnchor)
-    ])
-  }
-  
-  private func setupLabels() {
-    guard let avatarImageView = avatarImageView else { return }
-    let nameLabel = UILabel()
-    nameLabel.text = "Екатерина Новикова"
-    nameLabel.font = UIFont.systemFont(ofSize: 23, weight: .bold)
-    nameLabel.textColor = .white
-    
-    let loginNameLabel = UILabel()
-    loginNameLabel.text = "@ekaterina_nov"
-    loginNameLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
-    loginNameLabel.textColor = .ypGray
-    
-    let descriptionLabel = UILabel()
-    descriptionLabel.text = "Hello, world!"
-    descriptionLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
-    descriptionLabel.textColor = .white
-    
-    let stackView = UIStackView()
-    stackView.distribution = .fill
-    stackView.axis = .vertical
-    stackView.spacing = 8
-    stackView.translatesAutoresizingMaskIntoConstraints = false
+  private func addViews() {
+    view.addSubview(avatarImageView)
+    view.addSubview(logoutButton)
     view.addSubview(stackView)
-
-    NSLayoutConstraint.activate([
-      stackView.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 10),
-      stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-      stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
-    ])
-
     stackView.addArrangedSubview(nameLabel)
     stackView.addArrangedSubview(loginNameLabel)
     stackView.addArrangedSubview(descriptionLabel)
+  }
+  
+  private func activateConstraints() {
+    NSLayoutConstraint.activate([
+      avatarImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+      avatarImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+      logoutButton.centerYAnchor.constraint(equalTo: avatarImageView.centerYAnchor),
+      logoutButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+      stackView.leadingAnchor.constraint(equalTo: avatarImageView.leadingAnchor),
+      stackView.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 8),
+      
+    ])
   }
 }
