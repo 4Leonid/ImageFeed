7e1cc236a43ed4bb2201a38d75edbb07dd299658
Leonid <leo-turko@mail.ru>
2023-03-22 19:24:49 +0300

    Add Progress View

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -19,6 +19,7 @@ final class WebViewViewController: UIViewController {
 
   //  MARK: - IB Outlets
   @IBOutlet private var webView: WKWebView!
+  @IBOutlet private var progressView: UIProgressView!
   
   weak var delegate: WebViewViewControllerDelegate?
   
@@ -36,6 +37,38 @@ final class WebViewViewController: UIViewController {
     
     let request = URLRequest(url: url)
     webView.load(request)
+    
+    updateProgress()
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    webView.addObserver(
+      self,
+      forKeyPath: #keyPath(WKWebView.estimatedProgress),
+      options: .new, context: nil)
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    webView.removeObserver(
+      self,
+      forKeyPath: #keyPath(WKWebView.estimatedProgress),
+      context: nil
+    )
+  }
+  
+  override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+    if keyPath == #keyPath(WKWebView.estimatedProgress) {
+      updateProgress()
+    } else {
+      super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+    }
+  }
+  
+  private func updateProgress() {
+    progressView.progress = Float(webView.estimatedProgress)
+    progressView.isHidden = fabs(webView.estimatedProgress - 1.0) <= 0.0001
   }
 
   @IBAction private func didTapBackButton() {
